4e4e8a5ae3de636e1392480481d4d0fe42304eae
Andreas Siegemund <hausensausen@gmail.com>
2025-07-16 14:51:43 +0200

    set base design

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use slint::ToSharedString;
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
-    let main_window = MainWindow::new()?;
-
-    main_window.run()
+    let app_window = AppWindow::new()?;
+    app_window.invoke_set_font_size(14.0);
+    app_window.invoke_set_font_family("comic sans".to_shared_string());
+    app_window.run()
 }
